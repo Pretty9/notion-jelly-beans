b8daf69a7f2243cacc885062bfc489c3a4637609
Pretty 9 <206367405@qq.com>
2021-10-09 17:03:47 +0800

    代码高亮

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,11 @@ import '../styles/globals.css'
 // core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css'
 
+// used for code syntax highlighting (optional)
+import 'prismjs/themes/prism-coy.css'
+
+// global style overrides for prism theme (optional)
+import '../styles/prism-theme.css'
 
 // global style overrides for notion
 import '../styles/notion.css'
