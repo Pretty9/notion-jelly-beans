84097d480bb41a580ec63c964686a2f1c9590e6b
Pretty 9 <206367405@qq.com>
2021-10-09 16:14:25 +0800

    临时保存

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import { config } from '../lib/config'
 import NotionPage from '../components/NotionPage'
 import Notion from '../lib/notion'
